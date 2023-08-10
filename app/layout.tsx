99430bfa9bd01adab0d17b89b9674f1740264fe6
Gerke van Garderen <info@gerkevangarderen.nl>
2023-08-10 17:03:51 +0200

    Add meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="nl">
+
+      <meta name="description" content="Normale werktijden in de accountancy? Ja tuurlijk!" />
+
+      {/* Facebook Meta Tags */}
+      <meta property="og:url" content="https://flow.joinsonandspice.nl" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Is er nog iemand op kantoor?" />
+      <meta property="og:description" content="Normale werktijden in de accountancy? Ja tuurlijk!" />
+      <meta property="og:image" content="https://images.joinsonandspice.nl/opengraph/js.png" />
+
+      {/* Twitter Meta Tags */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="flow.joinsonandspice.nl" />
+      <meta property="twitter:url" content="https://flow.joinsonandspice.nl" />
+      <meta name="twitter:title" content="Is er nog iemand op kantoor?" />
+      <meta name="twitter:description" content="Normale werktijden in de accountancy? Ja tuurlijk!" />
+      <meta name="twitter:image" content="https://images.joinsonandspice.nl/opengraph/js.png" />
+
       <body className={inter.className}>{children}</body>
     </html>
   )
